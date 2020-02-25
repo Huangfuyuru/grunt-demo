cc49177706cf023079d57c6313b7b4e8df6fb788
huangfuyuru <864134710@qq.com>
2020-02-25 17:10:38 +0800

    csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
 module.exports = function(grunt){
   grunt.initConfig({
-    htmlhint:{
+    csslint:{
       //option是选项的意思
       options:{
-        htmlhintrc:'./.htmlhintrc'
+        csslintrc:'./.csslintrc'
       },
-      src:['*.html']
+      src:['*.css']
     }
   });
 
-  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
 
-  grunt.registerTask('default',['htmlhint']);
+  grunt.registerTask('default',['csslint']);
 };
