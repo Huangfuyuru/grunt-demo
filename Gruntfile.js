6d0c3e2c42656b866f0274f633e09f359e5ae641
huangfuyuru <864134710@qq.com>
2020-03-04 09:41:03 +0800

    customize

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,5 @@
 module.exports = function(grunt){
-  grunt.initConfig({
-      sprite:{
-      files:{
-        src:['images/*.png'],
-        dest:'dist/bundle.png',
-        destCss:'dist/sprite.css'
-      }
-    } 
-  });
- 
-  grunt.loadNpmTasks('grunt-spritesmith');
-  grunt.registerTask('default',['sprite'])
+  grunt.registerTask('build','buld task',function(){
+    console.log('build task!')
+  })
 }
